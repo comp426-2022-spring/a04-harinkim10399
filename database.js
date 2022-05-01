e6795c9faf9d05e646576af34690015c503078ed
Harin <harinkim@live.unc.edu>
2022-05-01 17:05:39 -0400

    pls work

---
+++ database.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const Database = require('better-sqlite3');
+
+const db = new Database('log.db');
+
+const stmt = db.prepare(`
+    SELECT name FROM sqlite_master WHERE type='table' and 'access';`
+    );
+let row = stmt.get();
+if (row === undefined) {
+    console.log('Log database appears to be empty. I will initialize it now.')
+    const sqlInit = `
+        CREATE TABLE access ( id INTEGER PRIMARY KEY, 
+            remote-addr VARCHAR, 
+            remote-user VARCHAR, 
+            datetime VARCHAR, 
+            method VARCHAR, 
+            url VARCHAR, 
+            protocol TEXT
+            http-version NUMERIC, 
+            status INTEGER, 
+            referer: TEXT,
+            useragent: TEXT)`;
+    db.exec(sqlInit)
+    console.log('Your database has been initialized.')
+} else {
+    console.log('Database exists.')
+}
+
+module.exports = db
--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+const http = require("http");
+
+// Require Express.js
+const express = require('express');
+const app = express();
+const db = require('./database')
+const md5 = require("md5")
+const morgan = require('morgan')
+const fs = require('fs')
+// Use morgan for logging
+app.use(morgan('combined'))
+
 // Require minimist module
-const args = require('minimist')(process.argv.slice(2))
+const args = require('minimist')(process.argv.slice(2));
+args['port'];
+const port = args.port || process.env.PORT || 5000;
+
+args['debug']
+const debug = args.debug || 'false'
+
+args['log']
+const log = args.log || 'true'
+
+args['help']
+
 // See what is stored in the object produced by minimist
 console.log(args)
 // Store help text 
@@ -23,4 +46,112 @@ server.js [options]
 if (args.help || args.h) {
     console.log(help)
     process.exit(0)
+}
+
+// Start an app server
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
+});
+
+if (log == 'true') {
+    // Use morgan for logging to files
+    // Create a write stream to append (flags: 'a') to a file
+    const WRITESTREAM = fs.createWriteStream('access.log', { flags: 'a' })
+    // Set up the access logging middleware
+    app.use(morgan('combined', { stream: WRITESTREAM }))
+}
+
+app.use((req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers['referer'],
+        useragent: req.headers['user-agent']
+    }
+    next();
+})
+
+
+
+app.get('/app/', (req, res) => {
+    res.status(200).end("OK");
+    res.type("text/plain");
+});
+
+app.get('/app/', (req, res) => {
+    const statusCode = 200
+    const statusMessage = 'OK'
+    res.status(statusCode).end(statusCode + ' ' + statusMessage)
+});
+
+app.get('/app/flip', (req, res) => {
+    res.status(200).json({
+        "flip": coinFlip()
+    });
+});
+
+app.get('/app/flips/:number', (req, res) => {
+    const raw = coinFlips(req.params.number);
+    const summary = countFlips(raw);
+    res.status(200).json({
+        "raw": raw,
+        "summary": summary
+    });
+});
+
+app.get('/app/flip/call/:call', (req, res) => {
+    res.status(200).json(flipACoin(req.params.call))
+});
+
+
+// Default response for any other request
+app.use(function (req, res) {
+    res.status(404).send('404 NOT FOUND')
+});
+
+// Coin functions from a02
+function coinFlip() {
+    let result = Math.random();
+    if (result < 0.5) {
+        result = "heads";
+    } else {
+        result = "tails";
+    }
+    return result;
+}
+
+function coinFlips(flips) {
+    let array = [];
+    for (let i = 0; i < flips; i++) {
+        array.push(coinFlip());
+    }
+    return array;
+}
+
+function countFlips(array) {
+    const result = { heads: 0, tails: 0 };
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == "heads") {
+            result.heads++;
+        } else {
+            result.tails++;
+        }
+    }
+    return result;
+}
+
+function flipACoin(call) {
+    const result = { call: call, flip: coinFlip(), result: "" };
+    if (result.call == result.flip) {
+        result.result = "win";
+    } else {
+        result.result = "lose";
+    }
+    return result;
 }
