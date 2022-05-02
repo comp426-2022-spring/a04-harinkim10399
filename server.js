250eedf13bb8caf9f9ca541bfc094497fa8a872b
Harin <harinkim@live.unc.edu>
2022-05-02 03:53:45 -0400

    dfsfa

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use((req, res, next) => {
     }
     console.log(logdata)
     const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referrer, logdata.useragent)
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
     next();
 })
 
